99b40ee7aa29c89e9b16b63082f39f8e9b199af2
ThibautNottebaere <thibaut.nottebaere@ugent.be>
2025-11-19 15:05:44 +0100

    Update 1 binaire zoekmethode.py

--- 1 binaire zoekmethode.py
+++ 1 binaire zoekmethode.py
@@ -4,3 +4,6 @@ def zoek(list, x):
             return i
     else:
         return None
+
+#https://github.com/UGent-AlgoData-BusinessEngineering/Code_lectures_2526
+#https://drive.google.com/file/d/1Cus_CwCHw2M-k_nTm5aNr7EM61T6N_4S/view?usp=sharing
